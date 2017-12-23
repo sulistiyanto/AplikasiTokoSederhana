37d9254c6d0dcf40c399af1a1b888a04e7b6fe24
sulistiyanto <sulistiyanto404@gmail.com>
2017-12-23 18:26:38 +0700

    Initial commit

---
+++ app/src/main/java/com/tubandev/aplikasitokosederhana/ui/login/LoginActivity.java
@@ -0,0 +1,22 @@
+package com.tubandev.aplikasitokosederhana.ui.login;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.tubandev.aplikasitokosederhana.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+}
